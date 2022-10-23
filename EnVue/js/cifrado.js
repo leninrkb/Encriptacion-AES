7f75147fab74982fc62e6b17a51031ac5de7d9a0
leninrkb <rkb.lenin@gmail.com>
2022-10-22 23:11:31 -0500

    funcionando todo

--- EnVue/js/cifrado.js
+++ EnVue/js/cifrado.js
@@ -1,29 +1,87 @@
+//global
+function encodeBase64(value) {
+    var words = CryptoJS.enc.Utf8.parse(value.toString());
+    return CryptoJS.enc.Base64.stringify(words);
+
+}
+
+function decodeBase64(value) {
+    var words = CryptoJS.enc.Base64.parse(value);
+    return CryptoJS.enc.Utf8.stringify(words);
+}
+
+
+
+//texto
 const cifrando = new Vue({
-    el:'#cifrado_texto',
-    data:{
-        contrasena:'',
-        texto_cifrar:'',
-        cifrado:'',
-        decifrado:'',
+    el: '#cifrado_texto',
+    data: {
+        contrasena: '',
+        texto_cifrar: '',
+        cifrado: '',
     },
-    methods:{
-        cifrar(){
-            this.cifrado = CryptoJS.AES.encrypt(this.texto_cifrar, this.contrasena).toString();            
+    methods: {
+        cifrar() {
+            this.cifrado = CryptoJS.AES.encrypt(this.texto_cifrar, this.contrasena).toString();
+            this.cifrado = encodeBase64(this.cifrado);
+        }
+    }
+})
+const descifrando = new Vue({
+    el: '#descifrado_texto',
+    data: {
+        contrasena: '',
+        cifrado: '',
+        descifrado: '',
+    },
+    methods: {
+        descifrar() {
+            this.cifrado = decodeBase64(this.cifrado);
+            this.descifrado = CryptoJS.AES.decrypt(this.cifrado, this.contrasena).toString(CryptoJS.enc.Utf8);
         }
     }
 })
 
-const decifrando = new Vue({
-    el:'#decifrado_texto',
-    data:{
-        contrasena:'',
-        cifrado:'',
-        decifrado:'',
+
+//archivo
+const cifrando_archivo = new Vue({
+    el: '#cifrado_archivo',
+    data: {
+        contrasena: '',
+        archivo: null,
+        cifrado: '',
     },
-    methods:{
-        decifrar(){
-            this.decifrado = CryptoJS.AES.decrypt(this.cifrado, this.contrasena).toString(CryptoJS.enc.Utf8);
-            //this.decifrado = this.cifrado + " " + this.contrasena;
+    methods: {
+        cargarArchivo(event) {
+            this.archivo = event.target.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(this.archivo);
+            reader.onload = () => {
+                const datos = reader.result;
+                this.archivo = datos;
+            }
+        },
+        cifrar() {
+            this.cifrado = CryptoJS.AES.encrypt(JSON.stringify(this.archivo), this.contrasena).toString();
+            this.cifrado = encodeBase64(this.cifrado);
+        }
+    }
+})
+const descifrando_archivo = new Vue({
+    el: '#descifrado_archivo',
+    data: {
+        contrasena: '',
+        cifrado: '',
+        descifrado: null,
+        downloadableUrl:'',
+    },
+    methods: {
+        descifrar() {
+            this.cifrado = decodeBase64(this.cifrado);
+            this.descifrado = JSON.parse(CryptoJS.AES.decrypt(this.cifrado, this.contrasena).toString(CryptoJS.enc.Utf8)).split(',');
+            var blob = new Blob([decodeBase64(this.descifrado[1].toString())], { type: this.descifrado[0].toString() });
+            this.downloadableUrl = URL.createObjectURL(blob);
+            console.log(blob);
 
         }
     }
